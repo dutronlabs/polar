65cbabdf72f2647a7a2a290ed166d64b05bcd072
Chris Dutra <chris@dutronlabs.com>
2016-04-26 16:05:16 -0400

    Update polar.go

--- polar.go
+++ polar.go
@@ -1,9 +1,22 @@
-package polar
+package main
 import "github.com/gin-gonic/gin"
 func main(){
     router := gin.Default()
     router.GET("/ping", func(c *gin.Context) {
         c.String(200, "pong")
     })
+    
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+            "title":"polar"
+        })  
+    })
+    
+    router.GET("/scans", func(c *gin.Context){
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+            "title":"scans"
+        })
+    })
+    
     router.Run()
 }
